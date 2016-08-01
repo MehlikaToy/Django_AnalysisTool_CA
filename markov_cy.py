87aa1d2e80f289001976abcbebfb970e072ad01d
tejpalv8 <tejpalv8@gmail.com>
2016-08-01 13:28:19 -0700

    fix

--- markov_cy.py
+++ markov_cy.py
@@ -150,19 +150,19 @@ def markovMain(age = 49, total_stages = 40, endemicity = 3, stage_timeFrame = 1,
         cumulativeCost += sumCost(newList, curr_stage, total_stages)
         cumulativeQALY += sumUtility(newList, curr_stage, age, total_stages)
 
-        # print '\nCUMM Current Stage:', curr_stage-1, 'Age:', age
-        # try:
-        #     print 'HCC : ', getCummDict('HCC')
-        #     print 'HCC NH : ', getCummDict('HCC NH')
-        #     print 'Death HBV: ', getCummDict('Death HBV')
-        #     print 'Death HBV NH:', getCummDict('Death HBV NH')
-        #     print 'Cirrhosis: ', cummCirr * cohortPop
-        #     print 'Cirrhosis NH: ', getCummDict('Cirrhosis NH')
-        #     print 'Cirrhosis Total:', (cummCirr * cohortPop) + getCummDict('Cirrhosis NH')
-        # except:
-        #     pass
+        print '\nCUMM Current Stage:', curr_stage-1, 'Age:', age
+        try:
+            print 'HCC : ', getCummDict('HCC')
+            print 'HCC NH : ', getCummDict('HCC NH')
+            print 'Death HBV: ', getCummDict('Death HBV')
+            print 'Death HBV NH:', getCummDict('Death HBV NH')
+            print 'Cirrhosis: ', cummCirr * cohortPop
+            print 'Cirrhosis NH: ', getCummDict('Cirrhosis NH')
+            print 'Cirrhosis Total:', (cummCirr * cohortPop) + getCummDict('Cirrhosis NH')
+        except:
+            pass
 
-        # print '\n#######################'
+        print '\n#######################'
 
 
     output = {}
@@ -191,7 +191,7 @@ def markovMain(age = 49, total_stages = 40, endemicity = 3, stage_timeFrame = 1,
     except:
         pass
 
-
+    markovMain(age = 49, total_stages = 40, endemicity = 3, stage_timeFrame = 1, initialList=[])
 
     return {'output': output, 'finalList': finalList, 'DeathHBV': DeathHBV, 'Cirrhosis': Cirrhosis, 'HCC': HCC, 'LT': LT}
 
